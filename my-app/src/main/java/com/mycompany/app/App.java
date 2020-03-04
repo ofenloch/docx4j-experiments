f6e6df0a85ae5310394e1ee88e34eb8d2c3c12ed
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-03-04 19:51:20 +0100

    Do some very simple stuff with docx4j

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,25 @@
 package com.mycompany.app;
 
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello, World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // Do some very simple stuff with docx4j:
+        String inputfilepath = "data/sample.docx";
+        try {
+            WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(new java.io.File(inputfilepath));
+            System.out.println("Opened file \"" + inputfilepath + "\".");
+            System.out.println("Document's title is \"" + wordMLPackage.getTitle() + "\".");
+        } catch (Docx4JException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
