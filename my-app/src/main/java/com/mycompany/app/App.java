599e339c7d348777164c651e991efc203c7a1ad7
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-03-04 20:30:56 +0100

    implement some basi logging to a FileAppender

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,11 @@
 package com.mycompany.app;
 
+// We do not use the ConsoleAppender
+// import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.SimpleLayout;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 
@@ -8,7 +14,29 @@ import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
  *
  */
 public class App {
+
+    private static Logger logger = Logger.getRootLogger();
+
     public static void main(String[] args) {
+
+        try {
+            SimpleLayout layout = new SimpleLayout();
+            // We do not use the ConsoleAppender
+            // ConsoleAppender consoleAppender = new ConsoleAppender(layout);
+            // logger.addAppender(consoleAppender);
+            FileAppender fileAppender = new FileAppender(layout, "./my-app.log", false);
+            logger.addAppender(fileAppender);
+            // ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF:
+            logger.setLevel(Level.INFO);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        logger.debug("My  test DEBUG message");
+        logger.info("My  test INFO message");
+        logger.warn("My  test WARN message");
+        logger.error("My  test ERROR message");
+        logger.fatal("My  test FATAL error message");
+
         System.out.println("Hello, World!");
 
         // Do some very simple stuff with docx4j:
